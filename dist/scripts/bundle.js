a67e962f6145fe3dd08d56de98711e70357c32ba
R. Anthony Porter <rah@sheenonline.biz>
2017-03-28 04:12:11 -0400

    Converted authorPage to Controller View.
    Broke out authorList into a dummy view.

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -46305,19 +46305,11 @@ module.exports = About;
 "use strict";
 
 var React = require('react');
-var AuthorApi = require('../../api/authorApi');
 
-var Authors = React.createClass({displayName: "Authors",
-    getInitialState: function() {
-        return {
-            authors: []
-        }
+var AuthorList = React.createClass({displayName: "AuthorList",
+    propTypes: {
+        authors: React.PropTypes.array.isRequired
     },
-
-    componentWillMount: function() {
-        this.setState({ authors: AuthorApi.getAllAuthors() });
-    },
-
     render: function() {
         var createAuthorRow = function(author) {
             return (
@@ -46329,15 +46321,13 @@ var Authors = React.createClass({displayName: "Authors",
         }
         return (
             React.createElement("div", null, 
-                React.createElement("h1", null, "Authors"), 
-
                 React.createElement("table", {className: "table"}, 
                     React.createElement("thead", null, 
                         React.createElement("th", null, "ID"), 
                         React.createElement("th", null, "Name")
                     ), 
                     React.createElement("tbody", null, 
-                        this.state.authors.map(createAuthorRow, this)
+                        this.props.authors.map(createAuthorRow, this)
                     )
                 )
             )
@@ -46345,9 +46335,42 @@ var Authors = React.createClass({displayName: "Authors",
     }
 });
 
-module.exports = Authors;
+module.exports = AuthorList;
 
-},{"../../api/authorApi":159,"react":158}],163:[function(require,module,exports){
+},{"react":158}],163:[function(require,module,exports){
+"use strict";
+
+var React = require('react');
+var AuthorApi = require('../../api/authorApi');
+var AuthorList = require('./authorList');
+
+var AuthorPage = React.createClass({displayName: "AuthorPage",
+    getInitialState: function() {
+        return {
+            authors: []
+        }
+    },
+
+    componentDidMount: function() {
+        if(this.isMounted()) {
+            this.setState({ authors: AuthorApi.getAllAuthors() });
+        }
+    },
+
+    render: function() {
+
+        return (
+            React.createElement("div", null, 
+                React.createElement("h1", null, "Authors"), 
+                React.createElement(AuthorList, {authors: this.state.authors})
+            )
+        );
+    }
+});
+
+module.exports = AuthorPage;
+
+},{"../../api/authorApi":159,"./authorList":162,"react":158}],164:[function(require,module,exports){
 "use strict";
 
 var React = require('react');
@@ -46373,7 +46396,7 @@ var Header = React.createClass({displayName: "Header",
 
 module.exports = Header;
 
-},{"react":158}],164:[function(require,module,exports){
+},{"react":158}],165:[function(require,module,exports){
 "use strict";
 
 var React = require('react');
@@ -46391,7 +46414,7 @@ var Home = React.createClass({displayName: "Home",
 
 module.exports = Home;
 
-},{"react":158}],165:[function(require,module,exports){
+},{"react":158}],166:[function(require,module,exports){
 $ = jQuery = require('jquery');
 var React = require('react');
 var Home = require('./components/homePage');
@@ -46428,4 +46451,4 @@ render();
 
 //React.render(<Home />, document.getElementById('app'));
 
-},{"./components/about/aboutPage":161,"./components/authors/authorPage":162,"./components/common/header":163,"./components/homePage":164,"jquery":1,"react":158}]},{},[165]);
+},{"./components/about/aboutPage":161,"./components/authors/authorPage":163,"./components/common/header":164,"./components/homePage":165,"jquery":1,"react":158}]},{},[166]);
